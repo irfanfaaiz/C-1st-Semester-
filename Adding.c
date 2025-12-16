6c03ba61cd6fe8501b07edd6be2cd416b7e9f964
irfanfaaiz <irfancollege16@gmail.com>
2025-12-16 09:45:20 +0530

    Add files via upload
    C Language 1st semester

---
+++ Adding.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main()
+{
+int a,b,sum;
+printf("Enter any two numbers:\n");
+scanf("%d%d",&a,&b);
+sum = a+b;
+printf("The sum of %d and %d is \n%d.",a,b,sum);
+return 0;
+}
---
+++ Average.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main()
+{
+int a,b,sum,avg;
+printf("Enter any two numbers:\n");
+scanf("%d%d",&a,&b);
+sum = a+b;
+avg = sum / 2
+printf("The avg of %d and %d is \n%d.",a,b,avg);
+return 0;
+}
